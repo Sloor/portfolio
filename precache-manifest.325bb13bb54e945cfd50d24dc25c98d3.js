a95c5be38db8839cbf6267bac5661950863f2a63
Aseel Abusal <aseel.abosul@gmail.com>
2019-01-19 20:52:55 +0200

    Updates

---
+++ precache-manifest.325bb13bb54e945cfd50d24dc25c98d3.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "f2eea2342781a13d8b5650726466762c",
+    "url": "/portfolio/static/media/robots.f2eea234.png"
+  },
+  {
+    "revision": "675c287a2a49b894802702912a3fdb81",
+    "url": "/portfolio/static/media/me.675c287a.jpg"
+  },
+  {
+    "revision": "adf6f77d03bbc72631c03403a959c320",
+    "url": "/portfolio/static/media/gradient.adf6f77d.png"
+  },
+  {
+    "revision": "a5e717f4318a45a2736e947d107d42e5",
+    "url": "/portfolio/static/media/facerecognition.a5e717f4.png"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "721b50504a3da5ba6a41",
+    "url": "/portfolio/static/js/main.721b5050.chunk.js"
+  },
+  {
+    "revision": "daaae07b4cf35ee3f5a3",
+    "url": "/portfolio/static/js/1.daaae07b.chunk.js"
+  },
+  {
+    "revision": "721b50504a3da5ba6a41",
+    "url": "/portfolio/static/css/main.a35bee70.chunk.css"
+  },
+  {
+    "revision": "7c58754ff86bf4d1db35b9fa664b0af2",
+    "url": "/portfolio/index.html"
+  }
+];
